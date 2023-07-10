6b6218d2a4a51cb14ec201d4446bc5704d0e538f
shikajiro <shikajiro@gmail.com>
2023-07-10 16:27:06 +0900

    labelは任意

--- src/main.ts
+++ src/main.ts
@@ -62,9 +62,6 @@ export const execArtifact = async (
   const labels = payload.pull_request?.labels
       ?.map((label:any) => label.name)
       ?.filter((name:any) => name === 'hurry' || name === '2days' || name === '2weeks') as string[];
-  if (labels?.length === 0) {
-    throw new Error("Can not find label.");
-  }
 
   const slackIds = convertToChatworkUsername([requestedGithubUsername], mapping);
   if (slackIds.length === 0) {
--- src/modules/chatwork.ts
+++ src/modules/chatwork.ts
@@ -76,19 +76,17 @@ export const ChatworkRepositoryImpl = {
     message: string,
     labels: string[]
   ): Promise<ChatworkPostResult> => {
-    const chatworkUrl = `https://api.chatwork.com/v2/rooms/${roomId}/tasks`;
-
     const isHurry = labels.find((label) => label === 'hurry');
     const is2days = labels.find((label) => label === '2days');
-    const is2weeks = labels.find((label) => label === '2weeks');
     let limit = 0;
     const now = new Date();
     if(isHurry !== undefined) {
-        limit = new Date(now.getFullYear(), now.getMonth()-1, now.getDate(), 23, 59, 59).getTime();
+      limit = new Date(now.getFullYear(), now.getMonth()-1, now.getDate(), 23, 59, 59).getTime();
     }else if(is2days !== undefined) {
-        limit = new Date(now.getFullYear(), now.getMonth()-1, now.getDate()+2, 23, 59, 59).getTime();
-    }else if(is2weeks !== undefined) {
-        limit = new Date(now.getFullYear(), now.getMonth()-1, now.getDate()+14, 23, 59, 59).getTime();
+      limit = new Date(now.getFullYear(), now.getMonth()-1, now.getDate()+2, 23, 59, 59).getTime();
+    }else{
+      // is2weeks or default
+      limit = new Date(now.getFullYear(), now.getMonth()-1, now.getDate()+14, 23, 59, 59).getTime();
     }
     const encodedParams = new URLSearchParams();
     encodedParams.set('body', message);
@@ -97,7 +95,7 @@ export const ChatworkRepositoryImpl = {
     encodedParams.set('limit_type', "date");
     core.info(`param ${encodedParams}`);
     const result = await axios.post<ChatworkPostResult>(
-      chatworkUrl,
+      `https://api.chatwork.com/v2/rooms/${roomId}/tasks`,
       encodedParams,
       {
         headers: { "X-ChatWorkToken": apiToken },
